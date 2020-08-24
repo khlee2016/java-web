b8e9b13c77f668018b82c9360b9d8185987e1042
khlee2016 <khlee@seoultech.ac.kr>
2020-08-24 16:32:01 +0900

    Update WebInitializer.java

--- src/main/java/com/mkyong/web/WebInitializer.java
+++ src/main/java/com/mkyong/web/WebInitializer.java
@@ -1,6 +1,6 @@
 package com.mkyong.web;
 
-import com.mkyong.web.config.SpringConfig;
+//import com.mkyong.web.config.SpringConfig;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -10,10 +10,10 @@ public class WebInitializer extends AbstractAnnotationConfigDispatcherServletIni
         return null;
     }
 
-    @Override
-    protected Class<?>[] getServletConfigClasses() {
-        return new Class[]{SpringConfig.class};
-    }
+//    @Override
+//    protected Class<?>[] getServletConfigClasses() {
+//        return new Class[]{SpringConfig.class}
+//    }
 
     @Override
     protected String[] getServletMappings() {
